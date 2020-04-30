d50bfc2074b12a9d4cb784495db3dbd5df39dbf2
SheepTester <ussa.say@gmail.com>
2020-04-29 18:30:04 -0700

    Render blocks, and prefer C-style for loops over range

--- src/utils/utils.mjs
+++ src/utils/utils.mjs
@@ -1,9 +1,17 @@
+// Performs a lot worse than a for loop; do not use
+// https://jsperf.com/sheeptester-for-loop
 export function* range (start, stop, step = 1) {
   if (stop === undefined) {
     stop = start
     start = 0
   }
-  for (let i = start; step < 0 ? i > stop : i < stop; i += step) {
-    yield i
+  if (step < 0) {
+    for (let i = start; i > stop; i += step) {
+      yield i
+    }
+  } else {
+    for (let i = start; i < stop; i += step) {
+      yield i
+    }
   }
 }
--- src/view/grid.mjs
+++ src/view/grid.mjs
@@ -1,5 +1,4 @@
 import { Vector2 } from '../utils/vector2.mjs'
-import { range } from '../utils/utils.mjs'
 
 const GRID_SIZE = 40
 
@@ -11,6 +10,21 @@ export class Grid {
     this.canvas = canvas
 
     this.scroll = scroll
+    this.blocks = new Set()
+  }
+
+  getBlock ({ x, y }) {
+    return this.blocks.has(`${x},${y}`)
+  }
+
+  placeBlock ({ x, y }) {
+    this.blocks.add(`${x},${y}`)
+    return this
+  }
+
+  removeBlock ({ x, y }) {
+    this.blocks.delete(`${x},${y}`)
+    return this
   }
 
   render () {
@@ -22,18 +36,29 @@ export class Grid {
     const start = this.scroll.clone().scale(1 / GRID_SIZE).map(Math.floor)
     const end = new Vector2(width, height).add(this.scroll).scale(1 / GRID_SIZE).map(Math.ceil)
     c.strokeStyle = 'rgba(255, 255, 255, 0.3)'
-    c.setLineDash([7, 7])
+    c.lineDashOffset = 2.5
+    c.setLineDash([5, 5])
     c.beginPath()
-    for (const lineX of range(start.x, end.x)) {
-      const x = lineX * GRID_SIZE - this.scroll.x
-      c.moveTo(x, -this.scroll.y % GRID_SIZE - GRID_SIZE)
-      c.lineTo(x, height)
+    for (let x = start.x; x < end.x; x++) {
+      const lineX = x * GRID_SIZE - this.scroll.x
+      c.moveTo(lineX, -this.scroll.y % GRID_SIZE - GRID_SIZE)
+      c.lineTo(lineX, height)
     }
-    for (const lineY of range(start.y, end.y)) {
-      const y = lineY * GRID_SIZE - this.scroll.y
-      c.moveTo(-this.scroll.x % GRID_SIZE - GRID_SIZE, y)
-      c.lineTo(width, y)
+    for (let y = start.y; y < end.y; y++) {
+      const lineY = y * GRID_SIZE - this.scroll.y
+      c.moveTo(-this.scroll.x % GRID_SIZE - GRID_SIZE, lineY)
+      c.lineTo(width, lineY)
     }
     c.stroke()
+
+    c.fillStyle = '#212121'
+    for (let x = start.x; x < end.x; x++) {
+      for (let y = start.y; y < end.y; y++) {
+        const block = new Vector2(x, y)
+        if (this.getBlock(block)) {
+          c.fillRect(...block.scale(GRID_SIZE).sub(this.scroll), GRID_SIZE, GRID_SIZE)
+        }
+      }
+    }
   }
 }
